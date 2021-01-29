620aa93742e87b0c3be7fca745443c00509d09dd
BogdanIelceanUiPath <75422739+BogdanIelceanUiPath@users.noreply.github.com>
2021-01-29 03:53:02 +0200

    Add project files.

---
+++ Models/Enrollment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebUniversity.Models
+{
+    public enum Grade
+    {
+        One = 1
+        , Two
+        , Three
+        , Four
+        , Five
+        , Six
+        , Seven
+        , Eight
+        , Nine
+        , Ten
+    }
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
+
+        public Grade? Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
